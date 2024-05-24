1092d182b258158500a879aca406c82cc6ac0f7d
Salim_AMRI <amri.salim.2013@gmail.com>
2024-05-24 15:57:55 +0200

    new version build

--- build/Test/Test-c/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etud/Bureau/HGS-CVRP/Test/Test-c
-# Build directory: /home/etud/Bureau/HGS-CVRP/build/Test/Test-c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
